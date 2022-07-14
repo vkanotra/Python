4c68753676d3eaf89de9d29ca49730aeea50a1d0
AzureAD\VaibhavKanotra <vaibhav.kanotra@alqimi.com>
2022-07-14 22:56:09 +0530

    sqlconnection commit

--- SQLconnection.py
+++ SQLconnection.py
@@ -8,7 +8,7 @@ sql_stmt = "select * from students"
 myCursor = mydb.cursor()      # cursor is used to access data, can be imagined as a pointer which points to the data
 myCursor.execute(sql_stmt)          # execute any sql statement here
 records = myCursor.fetchall()       # to store data in records, or fetchone to fetch first record
-
+print()
 for i in records:
     print(i[0], i[1])
     print(i)
