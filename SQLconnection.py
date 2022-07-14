2dc5f73dc570f503fd0ea8bddb340e028f07ab91
AzureAD\VaibhavKanotra <vaibhav.kanotra@alqimi.com>
2022-07-14 23:08:10 +0530

    sqlconnection commit 3

--- SQLconnection.py
+++ SQLconnection.py
@@ -9,7 +9,6 @@ myCursor = mydb.cursor()      # cursor is used to access data, can be imagined a
 myCursor.execute(sql_stmt)          # execute any sql statement here
 records = myCursor.fetchall()       # to store data in records, or fetchone to fetch first record
 print()
-print
 for i in records:
     print(i[0], i[1])
     print(i)
